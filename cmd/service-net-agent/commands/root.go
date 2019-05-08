9433a82c832bbadd73ddb79c11e9691f35dc1f71
Giel de Nijs <giel@daisho.group>
2019-05-08 09:22:48 -0700

    [NP-1284] Add actual handling of logging options.

--- cmd/service-net-agent/commands/root.go
+++ cmd/service-net-agent/commands/root.go
@@ -17,13 +17,19 @@ import (
 	"github.com/spf13/cobra"
 
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
 
-var debugLevel bool
-var consoleLogging bool
-var rootConfig = config.NewConfig()
+var (
+	debugLevel bool
+	consoleLogging bool
+	logToFile bool
+	logFile string
+	logOut io.Writer
+	rootConfig = config.NewConfig()
+)
 
 var rootCmd = &cobra.Command{
 	Use:   defaults.AgentName,
@@ -56,6 +62,10 @@ func init() {
 	rootCmd.PersistentFlags().String("cert", "", "File with certificate to use to connect to Edge Controller")
 	rootConfig.BindPFlag("controller.cert", rootCmd.PersistentFlags().Lookup("cert"))
 
+	rootCmd.PersistentFlags().BoolVar(&logToFile, "log", false, "Output to log file")
+	rootCmd.PersistentFlags().StringVar(&logFile, "logfile", filepath.Join(rootConfig.Path, defaults.LogFile), "File to write log to if --log is set")
+	rootCmd.MarkPersistentFlagFilename("logfile")
+
 	rootCmd.PersistentFlags().BoolVar(&debugLevel, "debug", false, "Set debug level")
 	rootCmd.PersistentFlags().BoolVar(&consoleLogging, "consoleLogging", false, "Pretty print logging")
 }
@@ -71,7 +81,22 @@ func Execute() {
 // Initialize the agent
 func Setup(cmd *cobra.Command) {
 	// Logging
-	SetupLogging()
+	// If log file is not specifically set, we should look in root path
+	if !cmd.Flag("logfile").Changed {
+		logFile = filepath.Join(rootConfig.Path, defaults.LogFile)
+	}
+	err := SetupLogging(logToFile, logFile)
+	if err != nil {
+		Fail(err, "failed setting up logging")
+	}
+
+	// Set logfile in config - used to properly set command line parameters
+	// for Windows (where we enable logging). We set this even if currently
+	// logging to file is not enabled, because we want to always set it
+	// when starting as a service.
+	// Ideally we would use the Windows event logger, but that requires
+	// wrapping ZeroLog, which is a whole different ballgame.
+	rootConfig.LogFile = logFile
 
 	// If config file is not specifically set, we should look in root path
 	if !cmd.Flag("config").Changed {
@@ -79,25 +104,55 @@ func Setup(cmd *cobra.Command) {
 	}
 
 	// Handle configuration
-	err := rootConfig.Read()
+	err = rootConfig.Read()
 	if err != nil {
 		Fail(err, "failed to read configuration file")
 	}
 }
 
 // SetupLogging sets the debug level and console logging if required.
-func SetupLogging() {
+func SetupLogging(toFile bool, file string) derrors.Error {
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	if debugLevel {
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	}
 
-	if consoleLogging {
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	if toFile {
+		err := os.MkdirAll(filepath.Dir(file), 0755)
+		if err != nil {
+			return derrors.NewPermissionDeniedError("unable to create log file directory", err).WithParams(file)
+		}
+		f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return derrors.NewPermissionDeniedError("unable to open log file for writing", err).WithParams(file)
+		}
+		logOut = f
+	} else {
+		logOut = os.Stderr
 	}
+
+	if consoleLogging {
+		logOut = zerolog.ConsoleWriter{Out: logOut}
+	}
+
+	log.Logger = log.Output(logOut)
+
+	return nil
 }
 
 func Fail(err derrors.Error, msg string) {
-	log.Debug().Str("err", err.DebugReport()).Msg("debug report")
-	log.Fatal().Err(err).Msg(msg)
+	var logger zerolog.Logger = log.Logger
+
+	// Make sure we print to stderr
+	if logToFile && logOut != nil {
+		var console io.Writer = os.Stderr
+		if consoleLogging {
+			console = zerolog.ConsoleWriter{Out: console}
+		}
+		out := io.MultiWriter(logOut, console)
+		logger = log.Output(out)
+	}
+
+	logger.Debug().Str("err", err.DebugReport()).Msg("debug report")
+	logger.Fatal().Err(err).Msg(msg)
 }
