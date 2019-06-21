2cbdeedae6f52ded001e1cb580fcfa623765fdf0
Giel de Nijs <giel@daisho.group>
2019-06-21 11:05:19 -0700

    [NP-1537] Add test for Disable

--- internal/app/run/service_test.go
+++ internal/app/run/service_test.go
@@ -48,4 +48,46 @@ var _ = ginkgo.Describe("service", func() {
 
 		gomega.Expect(testHandler.GetNumChecks()).To(gomega.BeNumerically(">=", cur + 2))
 	})
+	ginkgo.It("should start, run and disable and stop", func() {
+		s := Service{
+			Config: testConfig,
+			Client: testClient,
+		}
+
+		testConfig.Set("agent.asset_id", "test-asset")
+		testConfig.Set("agent.interval", "0.001s")
+		testConfig.Set("agent.opqueue_len", 10)
+		testConfig.Set("agent.shutdown_timeout", "10s")
+		testConfig.Set("agent.comm_timeout", "10s")
+
+		errChan := make(chan derrors.Error, 1)
+		derr := s.Start(errChan)
+		gomega.Expect(derr).To(gomega.Succeed())
+
+		// Wait for at least one heartbeat
+		for {
+			alive, derr := s.Alive()
+			gomega.Expect(derr).To(gomega.Succeed())
+			if alive {
+				break
+			}
+			time.Sleep(time.Second / 1000) // One heartbeat period
+		}
+
+		s.Disable()
+
+		// Should die but not stop
+		for {
+			alive, derr := s.Alive()
+			gomega.Expect(derr).To(gomega.Succeed())
+			if !alive {
+				break
+			}
+			time.Sleep(time.Second / 1000) // One heartbeat period
+		}
+
+		s.Stop()
+		derr = <-errChan // wait until done
+		gomega.Expect(derr).To(gomega.Succeed())
+	})
 })
